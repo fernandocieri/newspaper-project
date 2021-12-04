961bab11890011bc842504f9e6d0e53f37423812
josemsb92 <josemsb92@gmail.com>
2021-12-04 21:27:51 +0100

    React Project created

--- newspaper-project/src/index.js
+++ newspaper-project/src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+function HelloWorld(){
+  return <h1>Hello World</h1>;
+}
+
+const html = <HelloWorld />
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  html,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
