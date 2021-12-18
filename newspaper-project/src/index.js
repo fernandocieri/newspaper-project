c70659719e5298fa16c5f38a93a7ecbf7313eb36
josemsb92 <josemsb92@gmail.com>
2021-12-18 11:51:32 +0100

    v 1.0 NewsPaper

--- newspaper-project/src/index.js
+++ newspaper-project/src/index.js
@@ -128,7 +128,7 @@ const mockedNews = [
 class FullNewsArticle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...this.props.newsData };
+    this.state = { news:this.props.newsData, category:this.props.category };
     this.prevPage = this.prevPage.bind(this);
   }
 
@@ -140,13 +140,12 @@ class FullNewsArticle extends React.Component {
     return (
       <article id="fullArticle">
         <button onClick={this.prevPage}>-- volver</button>
-        <h2>{this.state.title}</h2>
-        <img src={this.state.urlToImage} />
+        <h2>{this.state.news.title}</h2>
+        <img src={this.state.news.urlToImage} />
         <div className="artextrainfo">
-         {this.state.author} - {this.state.publishedAt}
+          {this.state.news.author} - {this.state.news.publishedAt}
         </div>
-        <p>{this.state.content}</p>
-        <div className="artTheme">soy el tema</div>
+        <p>{this.state.news.content}</p>        
       </article>
     );
   }
@@ -155,7 +154,7 @@ class FullNewsArticle extends React.Component {
 class NewsArticlePreview extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...this.props.newsData };
+    this.state = { ...this.props.newsData, articleTheme: this.props.articleTheme };
     this.sendID = this.sendID.bind(this);
   }
 
@@ -163,59 +162,65 @@ class NewsArticlePreview extends React.Component {
   sendID() {
     this.props.handlePageChanges(this.state.identifier);
   }
+  // componentDidUpdate(prevProps) {    
+  //   if(prevProps.articleTheme !== this.props.articleTheme) {
+  //     this.forceUpdate()
+  //     console.log(`Estado PREVIO ${prevProps.articleTheme}`)
+  //     console.log(`Estado nuevo ${this.props.articleTheme}`)
+  //   }
+  // }  
 
-  render() {
+  // componentDidUpdate(prevProps){
+  //   if(prevProps.artTheme !== this.props.articleTheme){
+  //     this.setState({...this.props.newsData, articleTheme: this.props.articleTheme})
+  //   }
+  // }
+  render() {    
     return (
       <article onClick={this.sendID} className="article-preview">
-        <div className="artTitle">{this.state.title}</div>
-        <div className="artTheme">soy el tema</div>
-        <img src={this.state.urlToImage} />
-        <p>{this.state.description}</p>
+        <div className="artTitle">{this.props.newsData.title}</div>        
+        <img src={this.props.newsData.urlToImage} alt="Image not found"/>
+        <p>{this.props.newsData.description}</p>
         <button>leer más --</button>
         <div className="artextrainfo">
-        - {this.state.author} - {this.state.publishedAt}
+        - {this.props.newsData.author} - {this.props.newsData.publishedAt}
         </div>
       </article>
     );
   }
 }
-
-/*class NewsArticle extends React.Component {
-  render() {
-    return (
-      <div>
-
-      </div>
-    )
-  }
-}*/
-
 class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       news: [],
+      category: "general",
       articleIsOpen: false,
       showedArtID: undefined,
-      articleTheme: undefined,
+      articleTheme: "general",
     };
     this.handlePageChanges = this.handlePageChanges.bind(this);
     this.handleSetTheme = this.handleSetTheme.bind(this)
   }
 
   async componentDidMount() {
-    const resp = await axios.get('https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=e1c41041380c4911ad71f2375242b73d');
+    let category = this.state.category
+    const resp = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=e1c41041380c4911ad71f2375242b73d`);
     let apiNews = []
     for (let i = 0; i < resp.data.articles.length; i++) {
+      
       if (resp.data.articles[i].author === null || resp.data.articles[i].author === '') {
         resp.data.articles[i].author = 'anonymous author';
       }
-      apiNews.push({...resp.data.articles[i], identifier:i})
+      if(resp.data.articles[i].content !== null){
+        apiNews.push({...resp.data.articles[i], identifier:i})
+      }
+      
     }
-    this.setState(()=> ({news: apiNews}))
-    console.log(this.state.news);
-  }
+    this.setState(()=> ({news: apiNews, category: category}))    
+  }  
 
+  
   handlePageChanges(id) {
     this.setState((state) => ({
       articleIsOpen: !state.articleIsOpen,
@@ -223,31 +228,33 @@ class Main extends React.Component {
     }));
   }
 
-  handleSetTheme(theme) {
+  async handleSetTheme(theme) {    
+    let category = "general";
     this.setState((state) => ({ articleTheme: theme }));
     if (this.state.articleIsOpen === true) { this.setState((state) => ({ articleIsOpen: false })) };
-  }
+    if(theme !=="general"){
+      category = theme  
+    }    
+    const resp =  await axios.get(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=e1c41041380c4911ad71f2375242b73d`);
+    let apiNews = []
+    for (let i = 0; i < resp.data.articles.length; i++) {
+      if (resp.data.articles[i].author === null || resp.data.articles[i].author === '') {
+        resp.data.articles[i].author = 'anonymous author';
+      }
+      if(resp.data.articles[i].content !== null){
+        apiNews.push({...resp.data.articles[i], identifier:i})
+      }     
+    }   
+    this.setState(()=> ({news: apiNews}))    
+    }    
 
-  render() {
-
-    const explorePage = (
-      <section className="explore">
+  render() {    
+    const filteredExplorePage = (
+      <section className="filtered-explore">
         {this.state.news.map((article) => (
           <NewsArticlePreview
             newsData={article}
-            handlePageChanges={this.handlePageChanges}
-            key={article.idNumber} /*key={mockedNews.indexOf(article)}*/
-          />
-        ))}
-      </section>
-    );
-
-    const articlesByTheme = this.state.news.filter((article) => { return article.artTheme === this.state.articleTheme });
-    const filteredExplorePage = (
-      <section className="filtered-explore">
-        {articlesByTheme.map((article) => (
-          <NewsArticlePreview
-            newsData={article}
+            articleTheme = {this.state.articleTheme}
             handlePageChanges={this.handlePageChanges}
             key={article.identifier} /*key={mockedNews.indexOf(article)}*/
           />
@@ -255,61 +262,47 @@ class Main extends React.Component {
       </section>
     );
 
+
     const clickedArticleSearch = this.state.news.filter((article) => { return article.identifier === this.state.showedArtID });
     const articlePage = (
       <FullNewsArticle
         handlePageChanges={this.handlePageChanges}
         newsData={clickedArticleSearch[0]}
       />
+      
     );
-
     //When the user clicks on a theme or the 'volver' button, the value of articleTheme will change;
-    /*when {articleIsOpen == true} explorePage or filteredExplorePage will hide and articlePage will display, 
-    getting the info needed to render by filtering mockedNews using showedArtID;*/
-    let currentRender = undefined;
-    if ((this.state.articleTheme === undefined) || (this.state.articleTheme === 'inicio')) {
-      currentRender = this.state.articleIsOpen ? articlePage : explorePage;
-    } else {
-      currentRender = this.state.articleIsOpen ? articlePage : filteredExplorePage;
-    }
-
+    /*when {articleIsOpen == true} or filteredExplorePage will hide and articlePage will display, 
+    getting the info needed to render by filtering mockedNews using showedArtID;*/         
+    let currentRender = this.state.articleIsOpen ? articlePage : filteredExplorePage;      
     return (
       <>
-        <Header handleSetTheme={this.handleSetTheme} />
+        <Header handleSetTheme={this.handleSetTheme} theme={this.state.articleTheme} />
         {currentRender}
       </>
     );
   }
 }
-
 //Class Header now contains a Class Navbar to make a Header for the web and include the nav on it.
 class Header extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = { news: [...mockedNews] };
+    super(props);    
     this.handleFilterThemes = this.handleFilterThemes.bind(this)
   }
   handleFilterThemes(artTheme) {
     this.props.handleSetTheme(artTheme);
   }
 
-  render() {
-    let rawThemes = this.state.news.map((article) => (
-      article.artTheme
-    ));
-    let filteredThemes = ['inicio'];
-    for (let element of rawThemes) {
-      if (!filteredThemes.includes(element)) {
-        filteredThemes.push(element);
-      }
-    }
+  render() {    
+    let filteredThemes = ['general',"sports","science","health","technology"];    
 
     return (
       <header>
         <h1>Chataca</h1>
         <nav>
-          {filteredThemes.map(theme => <Navbar handleFilterThemes={this.handleFilterThemes} newsData={theme} />)}
+          {filteredThemes.map(theme => <Navbar handleFilterThemes={this.handleFilterThemes} themes={theme} />)}
         </nav>
+        <p>TEMA: {this.props.theme}</p>
       </header>
     )
   }
@@ -317,16 +310,14 @@ class Header extends React.Component {
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { theme: this.props.newsData };
+    this.state = { theme: this.props.themes };
     this.sendTheme = this.sendTheme.bind(this);
-
   }
   sendTheme() {
     this.props.handleFilterThemes(this.state.theme);
   }
 
-  render() {
-    console.log(`Este ${this.state.theme}`);
+  render() {    
     return (
       <>
         <button onClick={this.sendTheme}>{this.state.theme}</button>
