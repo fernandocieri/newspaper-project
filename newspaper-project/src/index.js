ab924c334b16dfa0dda548e863d9307eae0f7f16
josemsb92 <josemsb92@gmail.com>
2021-12-19 01:07:56 +0100

    v 1.0 NewsPaper-proyect with comments

--- newspaper-project/src/index.js
+++ newspaper-project/src/index.js
@@ -2,5 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./assets/newspaper-explorer.css";
 import Main from "./components/main.js";
-
 ReactDOM.render(<Main />, document.getElementById("root"));
